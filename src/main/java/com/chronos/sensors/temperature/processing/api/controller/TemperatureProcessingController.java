744024c84b546415be804cbd2bc48731f9b71191
chronosinfo <chronosinfo@chronosinfo.com.br>
2025-05-07 10:50:05 -0300

    add rabbitmq

--- src/main/java/com/chronos/sensors/temperature/processing/api/controller/TemperatureProcessingController.java
+++ src/main/java/com/chronos/sensors/temperature/processing/api/controller/TemperatureProcessingController.java
@@ -1,7 +1,9 @@
 package com.chronos.sensors.temperature.processing.api.controller;
 
 import com.chronos.sensors.temperature.processing.api.controller.responses.TemperatureLogResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -10,11 +12,16 @@ import org.springframework.web.server.ResponseStatusException;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+import static com.chronos.sensors.temperature.processing.infrastructure.rabbitmq.RabbitMQConfig.FANOUT_EXCHANGE_NAME;
+
 
 @RestController
 @RequestMapping("/api/sensors/{sensorId}/temperatures/data")
 @Slf4j
+@RequiredArgsConstructor
 public class TemperatureProcessingController {
+
+    private final RabbitTemplate rabbitTemplate;
 
     @PostMapping(consumes = MediaType.TEXT_PLAIN_VALUE)
     public void data(@PathVariable UUID sensorId, @RequestBody String input) {
@@ -38,5 +45,12 @@ public class TemperatureProcessingController {
                 .build();
 
         log.info(logOutput.toString());
+
+        String exchange = FANOUT_EXCHANGE_NAME;
+        String routingKey = "";
+        Object payload = logOutput;
+
+        rabbitTemplate.convertAndSend(exchange, routingKey, payload);
+
     }
 }
